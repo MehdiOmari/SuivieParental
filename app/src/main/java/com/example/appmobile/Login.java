b4984ef49a01857e2ebf3758ab7865ce095c1810
MehdiOmari <Omarimehdi521@gmail.com>
2024-05-15 13:52:44 +0100

    login.java

--- app/src/main/java/com/example/appmobile/Login.java
+++ app/src/main/java/com/example/appmobile/Login.java
@@ -8,12 +8,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class Login extends AppCompatActivity {
+
+    Button btn_login;
+    FirebaseAuth auth;
+    EditText e1;
+    EditText e2;
+    EditText e3;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +33,15 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.login);
 
-        Button btn_login = findViewById(R.id.button);
+        btn_login = findViewById(R.id.button);
+        auth = FirebaseAuth.getInstance();
+        e1 = findViewById(R.id.emailedit);
+        e2 = findViewById(R.id.usernameedit);
+        e3 = findViewById(R.id.passewordedit);
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText e1 = findViewById(R.id.emailedit);
-                EditText e2 = findViewById(R.id.usernameedit);
-                EditText e3 = findViewById(R.id.passewordedit);
                 String emaillogin = e1.getText().toString();
                 String usernamelogin = e2.getText().toString();
                 String passewordlogin = e3.getText().toString();
@@ -46,7 +60,22 @@ public class Login extends AppCompatActivity {
 
 
                 Intent intent = new Intent(Login.this,MainActivity.class);
-                if(x && y) startActivity(intent);
+                if(x && y) {
+                    loginUser(emaillogin , passewordlogin);
+                }
+            }
+        });
+
+        }
+    private void loginUser(String email, String passeword){
+        auth.createUserWithEmailAndPassword(email, passeword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(Login.this, "succeful", Toast.LENGTH_SHORT).show();
+
+                }
+                else Toast.makeText(Login.this, "failed", Toast.LENGTH_SHORT).show();
             }
         });
     }
